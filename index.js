54a32f94037f28bac1b540b97509acd2ba3cac4f
Guanghui Li <sxlllslgh@mail.nwpu.edu.cn>
2024-06-02 17:11:44 +0800

    Initialize commit.

---
+++ index.js
@@ -0,0 +1,21 @@
+const core = require('@actions/core');
+
+async function fetchRecord() {
+    try {
+        const myHeaders = new Headers();
+        myHeaders.append('Accept', 'application/json');
+        myHeaders.append('Authorization', `Bearer ${core.getInput('access-token')}`);
+
+        const record = await fetch(`https://pub.orcid.org/v3.0/${core.getInput('orcid-id')}/record`, {headers: myHeaders});
+        const recordJson = await record.json();
+        for (const [i, work] of recordJson['activities-summary'].works.group.entries()) {
+            const workDetailed = await fetch(`https://pub.orcid.org/v3.0/${core.getInput('orcid-id')}/work/${work['work-summary'][0]['put-code']}`, {headers: myHeaders});
+            recordJson['activities-summary'].works.group[i]['work-summary'][0] = Object.assign({}, recordJson['activities-summary'].works.group[i]['work-summary'][0], await workDetailed.json());
+        }
+        core.setOutput('record', JSON.stringify(recordJson));
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+fetchRecord().finally();
